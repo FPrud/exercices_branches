bc4ed468ee1401eec76ea5e40d2d161cee0fbcc1
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-06-10 16:58:28 +0200

    ajout de l'exercice 4 desCrepes

---
+++ fonctions/exerciceDesCrepes.js
@@ -0,0 +1,4 @@
+const preparerPate = (farine, lait, oeufs, sucre) => {
+    return `Pâte prête avec ${farine}g de farine, ${lait}ml de lait, ${oeufs} oeufs et ${sucre}g de sucre.`
+};
+console.log(preparerPate(250, 500, 3, 50));
