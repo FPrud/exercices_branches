78ca86f7e436b65f21b13b4dffc7ac4b6b4debae
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-07-08 10:17:48 +0200

    correction des dates dans les noms de dossiers

---
+++ exercices_individuels/250807_compteurBoucles/script.js
@@ -0,0 +1,23 @@
+// Écris une boucle FOR qui affiche dans la console tous les nombres de 0 à 99.
+// Écris le même compteur avec une boucle WHILE.
+// Réflexion : Quel type de boucle te semble le plus approprié pour cette application ?
+// Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
+// Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
+
+/*
+//boucle for
+for (i = 0; i < 100; i++) {
+    console.log(i);
+}
+*/
+
+/*
+//boucle while
+let i = 0;
+while (i < 99) {
+    i++
+    console.log(i);
+}
+*/
+
+//la boucle la plus pertinente dans ce contexte me semble être la boucle for car elle est utilisable sans avoir de variable à déclarer au préalable
