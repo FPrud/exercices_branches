ba03d0c5a3e0cb710d8a382de776478f138f7fc4
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-07-08 11:34:27 +0200

    exercice 1 boucles

--- exercices_individuels/250807_compteurBoucles/script.js
+++ exercices_individuels/250807_compteurBoucles/script.js
@@ -4,20 +4,28 @@
 // Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
 // Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
 
-/*
 //boucle for
 for (i = 0; i < 100; i++) {
     console.log(i);
 }
-*/
 
-/*
 //boucle while
-let i = 0;
-while (i < 99) {
-    i++
-    console.log(i);
+let j = 0;
+while (j < 99) {
+    j++;
+    console.log(j);
 }
-*/
 
-//la boucle la plus pertinente dans ce contexte me semble être la boucle for car elle est utilisable sans avoir de variable à déclarer au préalable
+//la boucle la plus pertinente dans ce contexte me semble être la boucle for car elle est utilisable sans avoir de variable à déclarer au préalable.
+
+//compteur de 50 à 99
+for (k = 50; k < 100; k++) {
+    console.log(k);
+}
+
+//compteur de 49 à 1 (méthode while)
+let l = 50;
+while (l > 1) {
+    l--;
+    console.log(l);
+}
