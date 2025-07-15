adee2d7b738c159f6db713000b9594a6fa06f576
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-07-15 14:52:44 +0200

    Exercice 3 : Parcourir un fichier JSON (fin)

--- exercices_individuels/250715_ParcourirFichierJSON/script.js
+++ exercices_individuels/250715_ParcourirFichierJSON/script.js
@@ -6,20 +6,20 @@ const data = JSON.parse(content);
 // write your code bellow
 
 // 1.Pour commencer, affiche dans ta console le type de data pour vérifier qu’il corresponde bien au type object et non string
-// console.log(typeof (data));
+console.log(typeof (data));
 
 // // 2.Affiche dans la console, le premier objet du tableau d’objets
-// console.log(data[0]);
+console.log(data[0]);
 
 // // 3.Affiche dans la console, le nombre d’objects contenu dans le tableau
-// console.log(data.length);
+console.log(data.length);
 
 // // 4.Affiche dans la console, les informations de la candidate arrivée au 4e rang
-// for (const item of data) {
-//     if (item.ranking === 4){
-//         console.log(item)
-//     }
-// };
+for (const item of data) {
+    if (item.ranking === 4){
+        console.log(item)
+    }
+};
 
 // 5.Affiche dans la console, l’âge moyen des candidates
 let ageTotal = 0;
