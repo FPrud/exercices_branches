1f87a0dd47446006c20d1493fde2f844497e64a8
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-07-17 15:07:54 +0200

    API Seigneur des Anneaux

---
+++ exercices_individuels/250717_APISeigneurDesAnneaux/script.js
@@ -0,0 +1,8 @@
+async function fetchLOTRMovies() {
+    const res = await fetch("https://the-one-api.dev/v2/movie", { headers: { Authorization: 'bearer GXKQgofAy-eXooVgz2br' } });
+    const dataBase = await res.json();
+    for (const movie of dataBase.docs) {
+        document.querySelector("#ul").innerHTML += `<li><h2>${movie.name}</h2> a couté ${movie.budgetInMillions} millions de dollars à produire</li>`;
+    }
+};
+fetchLOTRMovies();
